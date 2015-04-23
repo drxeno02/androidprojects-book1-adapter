c29cee41c4e5847bfc55fff3b0a914efaa3dd4ec
drxeno02 <ljtatum@hotmail.com>
2015-04-23 04:40:20 -0700

    Update 04.23.15
    Minor update to adapter class

--- DRXenoAdapter/src/com/blog/ljtatum/drxenoadapter/adapter/FeedListAdapter.java
+++ DRXenoAdapter/src/com/blog/ljtatum/drxenoadapter/adapter/FeedListAdapter.java
@@ -94,7 +94,7 @@ public class FeedListAdapter extends ArrayAdapter<FeedStructure> {
 					Bitmap img = getBitmapFromURL(url);
 					holder.iv.setImageBitmap(img);
 				} else {
-					holder.iv.setBackgroundResource(R.drawable.img);
+					holder.iv.setImageResource(R.drawable.img);
 				}
 			}
 		} catch (MalformedURLException e) { 
